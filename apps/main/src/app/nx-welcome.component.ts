a84db184de86c573fad6d7148afc12024457fd0a
carolYin <carolyin111@gmail.com>
2024-10-17 19:41:26 +0800

    fix: use tailwindCSS

--- apps/main/src/app/nx-welcome.component.ts
+++ apps/main/src/app/nx-welcome.component.ts
@@ -5,7 +5,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-nx-welcome',
   standalone: true,
   imports: [CommonModule],
-  template: `<h1>Welcome</h1> <br> <h2>Hey</h2>`,
+  template: `<h1 class="text-red-500">Welcome</h1><span>---</span><h2>Hey</h2>`,
   styles: [],
   encapsulation: ViewEncapsulation.None,
 })
